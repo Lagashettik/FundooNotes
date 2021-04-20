f362155c87d2a72a291efd9a4959d859319193da
Lagashettik <69065246+Lagashettik@users.noreply.github.com>
2021-04-20 17:46:19 +0530

    Added: Project Initialisation and setup

---
+++ App.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import { View } from 'react-native';
+import Controler from './src/components/controler'
+
+const App = () => {
+  return (
+    <View>
+      <Controler/>
+    </View>
+  )
+}
+
+export default App;
---
+++ src/components/controler.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Controler extends Component {
+    render(){
+        return(
+            <Text>Welcome To FundooNotes</Text>
+        )
+    }
+}
